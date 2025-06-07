d09d0e7ebc2dc8a4c4830b358e022eb80ee8945b
vladimir-molotkov <molotkov1vl@gmail.com>
2025-06-07 23:00:36 +0300

    fix main

--- main.py
+++ main.py
@@ -9,38 +9,42 @@ from scripts.vanilla_gpt_benchmark import benchmark_gpt, train_gpt
 from utils.mps_enable import configure_mps
 
 
-@hydra.run_benchmarks(version_base=None, config_path="configs", config_name="config")
-def run_benchmarks(cfg: DictConfig):
+def run_benchmarks(bert_model_name, gpt_model_name, sample_size, batch_size):
     print("Benchmarking BERT Baseline")
     start = time.time()
     bert_loss = benchmark_bert(
-        model_name=cfg.model.bert_name,
-        sample_size=cfg.data.val_sample_size,
-        batch_size=cfg.training.batch_size,
+        model_name=bert_model_name,
+        sample_size=sample_size,
+        batch_size=batch_size,
     )
-    print(f"BERT Validation Loss: {bert_loss:.3f}. Time: {time.time() - start:.0f}s")
+    print(f"BERT Validation Loss: {bert_loss}. Time: {time.time() - start:.0f}s")
 
     print("\nBenchmarking Vanilla GPT")
     start = time.time()
     gpt_loss = benchmark_gpt(
-        model_name=cfg.model.gpt_name,
-        sample_size=cfg.data.val_sample_size,
-        batch_size=cfg.training.batch_size,
+        model_name=gpt_model_name,
+        sample_size=sample_size,
+        batch_size=batch_size,
     )
-    print(f"GPT Validation Loss: {gpt_loss:.3f}. Time: {time.time() - start:.0f}s")
+    print(f"GPT Validation Loss: {gpt_loss}. Time: {time.time() - start:.0f}s")
     return bert_loss, gpt_loss
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="config")
-def main(cfg: DictConfig, benchmark=True, train=True):
+def main(cfg: DictConfig):
     configure_mps()
     sample_size = cfg.data.val_sample_size
 
     # Initial benchmarks
-    if benchmark:
-        bert_loss, vanilla_gpt_loss = run_benchmarks()
+    if cfg.run_type.benchmark:
+        bert_loss, vanilla_gpt_loss = run_benchmarks(
+            cfg.model.bert_name,
+            cfg.model.gpt_name,
+            cfg.data.val_sample_size,
+            cfg.training.batch_size,
+        )
 
-    if train:
+    if cfg.run_type.train:
         print("\nTraining GPT Model")
         start = time.time()
         trained_model = train_gpt(
@@ -67,4 +71,5 @@ def main(cfg: DictConfig, benchmark=True, train=True):
 
 
 if __name__ == "__main__":
+    # main()
     fire.Fire(main)
